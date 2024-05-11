8f1428cbf7d7be2a7815ff82070b49b0eb0a81db
IvanS96-sar <ivan.shesterkin96@yandex.ru>
2024-05-11 13:35:19 +0300

    JDBC API - statement

--- src/main/java/ru/maxima/models/Person.java
+++ src/main/java/ru/maxima/models/Person.java
@@ -1,5 +1,8 @@
 package ru.maxima.models;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Person {
     private Long id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2,max = 50, message = "Name should be min2 and max 50")
+
     private String name;
+    @Min(value = 0, message = "Age should be 0 years")
     private Long age;
 }
